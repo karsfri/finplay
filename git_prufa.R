d806f0ed574c39b8420d5f209787ec8337a1f683
karsfri <kari.s.fridriksson@gmail.com>
2018-10-18 21:53:18 +0000

    nenniekki

--- git_prufa.R
+++ git_prufa.R
@@ -1,3 +1,13 @@
 # Prufa git 
 
 # Prófa að breyta
+
+# prófa að vista skjöl, etc
+data("AirPassengers")
+library(tidyverse)
+library(here)
+
+dir.create("prufa_folder_push")
+
+AirPassengers %>% 
+    saveRDS(here("prufa_git_subfolder", "prufa.rds"))
